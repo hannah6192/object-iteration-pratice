1f5fa9219b41060db6a9f3fd80669d5c32340259
hannah6192 <hannah6192@gmail.com>
2023-05-17 00:04:50 +0000

    finish

--- for-in-loops.js
+++ for-in-loops.js
@@ -1,11 +1,25 @@
 // Problem 1
 const person = { name: 'John', age: 30, city: 'New York' };
-
+for (const  element in person){
+    console.log(`${element} : ${person[element]}`);
+}
 
 
 // Problem 2
 const numbers = { a: 10, b: 20, c: 30 };
+ let sum = 0;
+for(const element in numbers){
+    sum += numbers[element];
+    //console.log(numbers[element] += numbers[element]);
+    
+} console.log(sum)
+
 
 
 // Problem 3
 const originalObj = { a: 1, b: 2, c: 3 };
+for(const element in originalObj){
+     originalObj[element] = originalObj[element] * 2;
+    //console.log(numbers[element] += numbers[element]);
+    
+} console.log(originalObj)
--- for-of-loops.js
+++ for-of-loops.js
@@ -1,10 +1,16 @@
-// Modify this object to  create you
 const awesomePerson = {
     name: 'Jowel',
     age: 28,
     hobbies: ['lacrosse', 'reading', 'cooking', 'sports'],
     isEngineer: true
 };
+console.log('key:', Object.keys(awesomePerson));
+console.log('value:', Object.values(awesomePerson));
+for (const [key,value] of  Object.entries(awesomePerson)){
+        console.log(`${key}:${value}`);
+        }
+
+
 
 
 
